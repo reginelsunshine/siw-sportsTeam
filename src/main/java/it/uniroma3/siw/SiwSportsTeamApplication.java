c8c1c9d602fa7513971216d53fefb9c1cc9886d4
Abdalla El Awad <abd.elawad@stud.uniroma3.it>
2024-08-31 21:47:41 +0200

    Initial commit

---
+++ src/main/java/it/uniroma3/siw/SiwSportsTeamApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwSportsTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwSportsTeamApplication.class, args);
+	}
+
+}
